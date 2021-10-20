6d7c2023044efbbf6efea722d5fb761b2ae4213b
OUYACHOU Younes <ouyachouyouness@gmail.com>
2021-10-20 15:50:51 +0200

    Add Exception

--- index.js
+++ index.js
@@ -4,51 +4,63 @@ const some = require("./dataset/survery_1.json");
 
 //En general
 app.get("/", function (req, res) {
-  res.send(" First Exo");
+  if (!some) {
+    res.status(404).send("course not found ! ");
+  } else {
+    res.send(" First Exo");
+  }
 });
 
 //Require all data
 app.get("/data_remuneration", (req, res) => {
-  res.send(some);
+  if (!some) {
+    res.status(404).send("course not found ! ");
+  } else {
+    res.send(some);
+  }
 });
 
 // Filter by one or more fields/attributes
 app.get("/data_remuneration/:id", (req, res) => {
-  console.log(some);
-
-  const salaire = some.filter(
-    (list) =>
-      list["What is your annual salary?"] === req.query.salaire &&
-      list["Please indicate the currency"] === req.query.indice
-  );
-  res.send(salaire);
+  if (!some) {
+    res.status(404).send("course not found ! ");
+  } else {
+    const salaire = some.filter(
+      (list) =>
+        list["What is your annual salary?"] === req.query.salaire &&
+        list["Please indicate the currency"] === req.query.indice
+    );
+    res.send(salaire);
+  }
 });
 
 // Route sort
 app.get("/data_remuneration", (req, res) => {
-  console.log(some);
+  if (!some) {
+    res.status(404).send("course not found ! ");
+  } else {
+    let data = JSON.parse(JSON.stringify(some));
 
-  let data = JSON.parse(JSON.stringify(some));
+    // sort by one or more fields/attributes
+    if (req.query.sort) {
+      const keys = req.query.sort.split(",");
+      keys.forEach((key) => {
+        data = data.sort((a, b) => (a[key] > b[key] ? 1 : -1));
+      });
 
-  // sort by one or more fields/attributes
-  if (req.query.sort) {
-    const keys = req.query.sort.split(",");
-    keys.forEach((key) => {
-      data = data.sort((a, b) => (a[key] > b[key] ? 1 : -1));
-    });
+      res.send(data);
+    }
 
-    res.send(data);
-  }
-
-  //return a set of scattered fields
-  if (req.query.fields) {
-    const fields = req.query.fields.split(",");
-    let element = data[Math.floor(Math.random() * data.length)];
-    let obj = {};
-    fields.forEach((field) => {
-      obj[field] = element[field];
-    });
-    res.send(obj);
+    //return a set of scattered fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",");
+      let element = data[Math.floor(Math.random() * data.length)];
+      let obj = {};
+      fields.forEach((field) => {
+        obj[field] = element[field];
+      });
+      res.send(obj);
+    }
   }
 });
 
